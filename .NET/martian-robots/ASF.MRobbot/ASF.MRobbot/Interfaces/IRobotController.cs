9452e67d7d50bed139a58e2651842204d259a042
Santiago <santiago_figueroa_@hotmail.com>
2025-05-07 13:40:20 +0100

    Implement RobotController and IRobotController interface
    Updated RobotController.cs to implement the IRobotController interface, including a constructor that initializes command handlers and an ExecuteInstructions method for processing commands.
    
    Created or modified IRobotController.cs to define the interface with the ExecuteInstructions method signature, ensuring a consistent contract for implementing classes.
    
    Renamed  Solution.

---
+++ .NET/martian-robots/ASF.MRobbot/ASF.MRobbot/Interfaces/IRobotController.cs
@@ -0,0 +1,14 @@
+﻿using ASF.MRobot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASF.MRobot.Interfaces
+{
+    public interface IRobotController
+    {
+        string ExecuteInstructions(Robot robot, string instructions);
+    }
+}
