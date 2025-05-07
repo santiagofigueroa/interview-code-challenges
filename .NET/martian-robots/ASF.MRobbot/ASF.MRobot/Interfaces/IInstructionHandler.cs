ae197850516024bf5dae6265d7a9290c1cc23d28
Santiago <santiago_figueroa_@hotmail.com>
2025-05-07 14:37:07 +0100

    Refactor project structure and update dependencies
    - Removed unnecessary sections from the solution file.
    - Deleted the `RobotController` class and its methods.
    - Simplified `Program.cs` to retain only the main output.
    - Added `Microsoft.Extensions.DependencyInjection` package.
    - Introduced `IInstructionHandler` interface for instruction handling.

---
+++ .NET/martian-robots/ASF.MRobbot/ASF.MRobot/Interfaces/IInstructionHandler.cs
@@ -0,0 +1,14 @@
+﻿using ASF.MRobot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASF.MRobot.Interfaces
+{
+    public interface IInstructionHandler
+    {
+        void Handle(Robot robot, MarsGrid grid);
+    }
+}
