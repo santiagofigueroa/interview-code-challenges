151432b7c8e94bebe98a7b25a707257b570e2547
Santiago <santiago_figueroa_@hotmail.com>
2025-05-13 12:31:02 +0100

    Added  Fine Model and Db set

---
+++ .NET/library/Model/Fine.cs
@@ -0,0 +1,17 @@
+﻿namespace OneBeyondApi.Model
+{
+    public class Fine
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid BorrowerId { get; set; }
+        public Borrower Borrower { get; set; }
+
+        public Guid BookId { get; set; }
+        public Book Book { get; set; }
+
+        public decimal Amount { get; set; }
+        public DateTime IssuedDate { get; set; } = DateTime.UtcNow;
+        public string Reason { get; set; } = "Late return";
+
+    }
+}
